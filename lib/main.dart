cba4e4585721d7a15b147988c76b6355e0191a10
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-03-07 12:41:37 +0700

    decoration text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,23 +10,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(appBar: AppBar(
-        title: Text("Fitur Text Field", style: TextStyle(color: Colors.white),),
+        title: const Text("Fitur Text Field", 
+        style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: Center(
+      body: const Center(
         child: Padding(
-          padding: const EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
           child: TextField(
             autocorrect: false,
-            autofocus: false,
-            enableSuggestions: true,
-            enableInteractiveSelection: true,
-            // enabled: false,
+            showCursor: true,
+            cursorColor: Colors.red,
+            // cursorWidth: 10,
+            // cursorHeight: 25,
+            // cursorRadius: Radius.circular(20),
+
+            textAlign: TextAlign.start,
+            textAlignVertical: TextAlignVertical.center,
+            textCapitalization: TextCapitalization.none,
+
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20,
+            ),
             obscureText: true,
-            obscuringCharacter: 'T',
-            keyboardType: TextInputType.phone,
-            // readOnly: true,
+
+            decoration: InputDecoration(
+              fillColor: Colors.amber,
+              filled: true,
+              icon: Icon(
+                Icons.person,
+                size: 35,
+              ),
+              border: OutlineInputBorder(),
+
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                ),
+              ),
+
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.red,
+                ),
+              ),
+              prefixIcon: Icon(Icons.add_a_photo),
+              suffixIcon: IconButton(icon: Icon(Icons.remove_red_eye),onPressed: null),
+              // prefixIcon: Icon(
+              //   Icons.person_add,
+              //   size: 35,
+              //   // color: Colors.blue,
+              // ),
+              // prefixText: "Name: ",
+              hintText: "Please input your name..",
+              hintStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+              labelText: "Full Name",
+              labelStyle: TextStyle(
+                color: Colors.black,
+              ),
+            ),
           ),
         ),
       ),
